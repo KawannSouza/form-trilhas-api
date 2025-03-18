50e21830e0b4dcf4edc41575553c0119f2ede2c0
Kawann <kawannmarcelo7@gmail.com>
2025-03-18 10:35:49 -0300

    feat: adding and setting cors

--- src/server.js
+++ src/server.js
@@ -1,9 +1,11 @@
 import express from 'express';
+import cors from 'cors';
 import candidateRoutes from './routes/candidateRoutes.js';
 
 const app = express();
 const PORT = 3000;
 
+app.use(cors());
 app.use(express.json());
 
 app.use("/users", candidateRoutes);
