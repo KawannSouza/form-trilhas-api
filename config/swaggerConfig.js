ca9ceae2483a572acd4122fa74c6564ef98bdbc1
Kawann <kawannmarcelo7@gmail.com>
2025-03-19 11:23:27 -0300

    fix: changing swagger docs

--- config/swaggerConfig.js
+++ config/swaggerConfig.js
@@ -13,7 +13,7 @@ const swaggerOptions = {
         },
         servers: [
             {
-                url: "http://localhost:3000",
+                url: "https://form-trilhas-api.onrender.com",
             },
         ],
     },
